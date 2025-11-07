6d9bf037c4b194041bb04c05f344b17779a100f8
Alex home PC <24abickerton@liverpoolmathsschool.org>
2025-11-07 13:50:43 +0000

    did stuff wooo

--- Caesar.cpp
+++ Caesar.cpp
@@ -1,24 +1,28 @@
 #include <iostream>
 #include <string>
 int main(){
+    std::string outputs[26];
     std::cout << "Please input your string:\n";
     std::string input;
     std::getline(std::cin, input);
     for (auto it = input.begin(); it < input.end(); it++){
         *it = std::toupper(*it);
     }
+    outputs[0] = input;
     for (int i = 1; i < 26; i++){
-        std::cout << "Shifting by " << i << ":\n";
-        for (char ch: input){
-            if (ch > 'Z' || ch < 'A'){
-                std::cout << ch;
-            } else if (ch + i > 'Z'){
-                std::cout  << char(ch + i -26);
+        for (int i = 0; i < input.size(); i++){
+            if (input[i] > 'Z' || input[i] < 'A'){
+                
+            } else if (input[i] + 1 > 'Z'){
+                input[i] =  char(input[i] + 1 -26);
             } else {
-                std::cout << char(ch+i);
+                input[i] =  char(input[i]+1);
             }
         }
-        std::cout << '\n';
+        outputs[i] = input;
+    }
+    for (std::string i: outputs){
+        std::cout << i << "\n\n";
     }
     return 0;
 }
